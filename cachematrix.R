a94db0c7ba6884bd0ca0a438a335561ba3d4d2b0
StephenWicks <swicks@me.com>
2014-10-26 13:05:34 -0400

    Re-write of code for programming assignment2
    initial attempt at two functions for rprog-008

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cacheInv assigns the inverse of a matrix
+## getInv fetches that inverted matrix value
+## setMat the value of a matrix
+## getMat retrieves the value of a matrix
 
-## Write a short comment describing this function
+## The function returns a list of 4 elements (set, get, setinv, getinv)
+## each of which is a function
 
 makeCacheMatrix <- function(x = matrix()) {
+     cache <- NULL
+     setMat <-function(matrixValue) {
+          x <<- matrixValue
+          cache <<- NULL
+     }
+     getMat <- function() x
+     cacheInv <- function(solve) cache <<- solve
+     getInv <- function() cache
+     list(setMat = setMat,
+          getMat = getMat,
+          cacheInv = cacheInv,
+          getInv = getInv)
 
 }
 
 
-## Write a short comment describing this function
+##This funtion will evaluate if two matricies are identical.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+matequal <- function(mat1, mat2)
+     is.matrix(mat1) && is.matrix(mat2) && dim(mat1) == dim(mat2) && all(mat1 == mat2)
+
+## Return a matrix that is the inverse of 'x' but first check if the matrix has changed.
+## If it has changed, compute its inverse, else, proceed to see if there is a cached value
+
+cacheSolve <- function(y, ...) {
+     mat1 <- y
+     mat2 <- x$getMat()
+     if (matequal(mat1, mat2)) {
+          message("The matrix has changed")
+          
+ ##since the matrix changed, we must compute the inverse of the new matrix and return it.         
+        
+          inverse <- solve(mat1, ...)
+          return(inverse)
+     }
+## Return a matrix that is the inverse of x using elements of the makeCasheMatrix function
+     inverse <- x$getInv()
+     if(!is.null(inverse)) {
+          message("getting cached data")
+          return(inverse)
+     }
+     data <- x$getMat()
+     inverse <- solve(data, ...)
+     x$cacheInv(inverse)
+     inverse
 }
